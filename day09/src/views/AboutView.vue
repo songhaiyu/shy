3df77a6a4d63b45929791f7a70b88c8a6ca7135c
songhaiyu <1740741001@qq.com>
2022-11-26 22:10:34 +0800

    作业

---
+++ day09/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <!-- 插值表达式//模板字符串//声明式渲染 -->
+    <h1>{{meg}}</h1>
+    <h1 v-bind:title="mcg">鼠标悬停</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      meg:'第六组',
+      mcg:'页面加载与'+new Date().toLocaleString()
+    }
+  },
+}
+</script>
