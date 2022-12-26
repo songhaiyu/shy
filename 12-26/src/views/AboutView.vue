dc1ec2e90f4dd2f2b45a42569e92e3bdac403442
songhaiyu <1740741001@qq.com>
2022-12-26 22:10:01 +0800

    作业

---
+++ 12-26/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="">
+		<h2>一个人的信息</h2>
+		<h3>姓名：{{ name }}</h3>
+		<h3>年龄：{{ age }}</h3>
+		<button @click="sayHello">12</button>
+	</div>
+</template>
+
+<script>
+	import { ref, reactive, toRefs } from 'vue';
+	import { useStore } from 'vuex';
+	import { useRouter, useRoute } from 'vue-router';
+	export default {
+		setup(props, ctx) {
+			let name = '张三';
+			let age = 18;
+			const store = useStore();
+			const router = useRouter();
+			const route = useRoute();
+			const data = reactive({});
+			function sayHello() {
+				alert(`我是${name}，我${age}岁了`);
+			}
+			return { ...toRefs(data), name, age, sayHello };
+			// return () => h('h2', 'I am studying Vue3.');
+		},
+	};
+</script>
+
+<style lang="scss"></style>
---
+++ 12-26/src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="home">
+		<h1>姓名: {{ text }}</h1>
+		<button @click="getAge">获取数据</button>
+		<h1>{{ a }}</h1>
+		<h2>{{ obj.name }}--{{ obj.sex }}</h2>
+	</div>
+</template>
+
+<script>
+	import { ref, reactive, toRefs } from 'vue';
+	import { useStore } from 'vuex';
+	import { useRouter, useRoute } from 'vue-router';
+	export default {
+		beforeCreate() {
+			console.log('beforeCreate');
+		},
+		created() {
+			console.log('created');
+		},
+		beforeMount() {
+			console.log('beforeMount');
+		},
+		mounted() {
+			console.log('mounted');
+		},
+		setup(props, ctx) {
+			console.log('setup');
+			const store = useStore();
+			const router = useRouter();
+			const route = useRoute();
+			const data = reactive({});
+			const a = ref(12);
+			const obj = ref({
+				name: '6',
+				sex: '0',
+			});
+			let person = reactive({
+				name: '张三',
+				age: 18,
+				job: {
+					type: '前端工程师',
+					salary: '30K',
+					a: {
+						b: {
+							c: 99,
+						},
+					},
+				},
+				hobby: ['抽烟', '喝酒', '烫头'],
+			});
+
+			let text = '张三';
+			const getAge = () => {
+				console.log('方法');
+			};
+			return { ...toRefs(data), text, getAge, a, obj, person };
+		},
+	};
+</script>
+
+<style lang="scss"></style>
